30a73ebf21ff6060fd4693289bb88d8d7867956b
Iris <iris.devillars@gmail.com>
2023-03-03 11:05:40 +0100

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,8 @@
 import { constants, utils } from "starknetid.js";
+import { Provider } from "starknet";
 
 function App() {
-  let starknetIdContract = utils.getStarknetIdContract(
-    constants.StarknetChainId.MAINNET,
-  );
-  let domainTest = utils.encodeDomain("test.stark");
-  console.log("domainTest", domainTest);
-
-  let decodeTest = utils.decodeDomain([domainTest]);
-  console.log("decodeTest", decodeTest);
-
-  return <>StarknetId contract : {starknetIdContract}</>;
+  return <div></div>;
 }
 
 export default App;
