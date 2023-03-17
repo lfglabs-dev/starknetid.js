f2778cc0c40c61c4902fc984c13a6973994b30f4
Fricoben <benjamin.flores.pro@gmail.com>
2023-03-17 08:53:47 +0100

    ref: add test and index

--- packages/core/__test__/utils.test.ts
+++ packages/core/__test__/utils.test.ts
@@ -1,22 +1,30 @@
 import { utils } from "../src/index";
 
-describe("encode & decode domains", () => {
-  test("encodeDomain should return encoded domain name", async () => {
-    const encoded = utils.encodeDomain("test.stark");
-    expect(encoded).toBe(BigInt(1068731n));
+function generateString(length: number, characters: string): string {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+const basicAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789-";
+const bigAlphabet = "这来";
+const totalAlphabet = basicAlphabet + bigAlphabet;
+
+describe("Should test encoding/decoding hooks 2500 times", () => {
+  it("Should test encode and decode functions with a random string", () => {
+    for (let index = 0; index < 2500; index++) {
+      const randomString = generateString(10, totalAlphabet);
+      expect(utils.decode(utils.encode(randomString))).toBe(randomString);
+    }
   });
 
-  test("encodeDomain should fail because domain not ending with .stark", async () => {
-    expect(() => utils.encodeDomain("test")).toThrowError(
-      "Domain is not a stark domain",
-    );
-    expect(() => utils.encodeDomain("test.st")).toThrowError(
-      "Domain is not a stark domain",
-    );
-  });
-
-  test("decodeDomain should return starknet id domain", async () => {
-    const decoded = utils.decodeDomain([BigInt(1068731n)]);
-    expect(decoded).toBe("test.stark");
+  it("Should test decode and encode functions with a number", () => {
+    for (let index = 0; index < 2500; index++) {
+      const decoded = utils.decode(BigInt(index));
+      expect(utils.encode(decoded).toString()).toBe(index.toString());
+    }
   });
 });
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@
  */
 export * as utils from "./utils";
 export * as constants from "./constants";
+export * as starknetIDNavigator from "./starknetIDNavigator";
